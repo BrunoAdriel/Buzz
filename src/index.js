0d10a106ed8516d88b90b70a6ed6a66ab3b3563a
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2023-09-03 16:59:31 -0300

    agrego del navbar

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import navbar from  './Navbar/navbar';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
